cba09c40d3be08fa6fd7c9b35fb0f7e4b54dccdc
zibuyin <natdrone101@gmail.com>
2025-05-20 20:33:08 +0100

    Added server code(WIP)

--- server/GenerateQuestions.py
+++ server/GenerateQuestions.py
@@ -2,3 +2,18 @@ import random
 
 # Pre Declear Variables
 Swapped_Count = 1 # Pairs Swapped
+Swapped_Keys = {}
+
+for i in Swapped_Count:
+    key = str(i)
+    value1 = random.randint(0,9)
+    
+    
+    Swapped_Keys = dict()
+
+def Generate_Values():
+    value1 = random.randint(0,9)
+    def Generate_Value2():
+        value2 = random.randint(0,9)
+        if value2 == value1:
+            return 
