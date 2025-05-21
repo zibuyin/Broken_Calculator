1f94f6741d75f5065876ca483d570cfe8b040e3f
J4mik <j4mikl@gmail.com>
2025-05-21 10:28:42 +0100

    calculator

--- react-port/src/App.js
+++ react-port/src/App.js
@@ -8,8 +8,20 @@ function App() {
     <div id="content">
       <h1>Broken Calulator</h1>
       <div id="main">
-        <div id="left" width="100px">{question()}</div>
-        <div id="middle"></div>
+        <div id="left">{question(5)}</div>
+        <div id="middle">
+          <div>7</div>
+          <div>8</div>
+          <div>9</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div style={{width: "220px"}}>0</div>
+          <div>.</div>
+        </div>
         <div id="right">
           <div id="settings"></div>
           <div id="leaderBoards"></div>
--- react-port/src/Components/question.js
+++ react-port/src/Components/question.js
@@ -1,5 +1,25 @@
-export function question() {
+export function question(numPairs) {
+    let dictionary = []
+
+    dictionary.length = numPairs
+
+    let key1 = 0
+    let key2 = 0
+
+    for (var i = 0; i < numPairs; ++i) {
+        key1 = Math.floor(Math.random()*9)
+        key2 = Math.floor(Math.random()*9)
+    
+        while (key2 == key1) {
+            key2 = Math.floor(Math.random()*9)
+        }
+    
+        dictionary[i] = [`'${i}': [${key1}, ${key2}]`]
+    }
+
     return(
-        <p>hi</p>
+        <div>
+            {dictionary}
+        </div>
     )
 }
